a7e25130caa0113eb070bca64590298923f14ada
Sidhant Bhatia <sibhatia@microsoft.com>
2023-06-06 16:12:22 -0400

    adding ability for users to get RCAs via API requests

---
+++ Api/Controllers/FabricOwlController.cs
@@ -0,0 +1,44 @@
+﻿using FabricOwl;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FabricOwlController : ControllerBase
+    {
+        Base owl = new Base();
+
+        [HttpGet("{eventInstanceIds}")]
+        public async Task<string> GetFabricOwl(string eventInstanceIds)
+        {
+            List<RCAEvents> simulEvents = owl.getRCA(eventInstanceIds);
+            string result = "";
+            foreach (var s in simulEvents)
+            {
+                result += JsonConvert.SerializeObject(s, Formatting.Indented) + "\n";
+            }
+            return result;
+        }
+
+        [HttpGet("{startTimeUTC}/{endTimeUTC}")]
+        public async Task<string> GetFabricOwl(DateTime startTimeUTC, DateTime endTimeUTC)
+        {
+
+            List<RCAEvents> simulEvents = owl.getRCA();
+            string result = "";
+            foreach (var s in simulEvents)
+            { 
+                if (s.TimeStamp >= startTimeUTC && s.TimeStamp <= endTimeUTC)
+                {
+                    result += JsonConvert.SerializeObject(s, Formatting.Indented) + "\n";
+                }
+            }
+
+            return result;
+            }
+
+    }
+}
